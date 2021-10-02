17243390dba8d44e338bc6dae5a900f598c6f583
Jurien Meerlo <codescapade@gmail.com>
2021-10-02 02:26:58 +0100

    added project.

---
+++ src/Main.hx
@@ -0,0 +1,10 @@
+import scenes.GameScene;
+
+import spirit.core.Game;
+
+class Main extends Game {
+
+  public function new() {
+    super(GameScene);
+  }
+}
---
+++ src/scenes/GameScene.hx
@@ -0,0 +1,10 @@
+package scenes;
+
+import spirit.core.Scene;
+
+class GameScene extends Scene {
+
+  public override function init() {
+    trace('game scene init');
+  }
+}
